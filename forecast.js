bce6eb1366ada9cb8072fcbedbde1328babf75b6
Gela Samsonidze <samosnidze7@yahoo.com>
2020-09-12 16:07:22 +0400

    wether app with accuWeather

---
+++ forecast.js
@@ -0,0 +1,30 @@
+const key = "ijAiGGoGTWqWRkUfjkzLksVANkOrGx01"; 
+
+//get weather information
+const getWeather = async (id) => {
+    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base+query);
+    const data = await response.json();
+    // console.log(data);
+    return data[0];
+
+}
+
+// Get city information 
+const getCity = async (city) => {
+    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base+query);
+    const data = await response.json();
+
+    return  data[0];
+}
+
+// getCity("manchester")
+//     .then(data => {
+//         return getWeather(data.Key);
+//     }).then(data => console.log(data))
+//     .catch(err => console.log(err));
